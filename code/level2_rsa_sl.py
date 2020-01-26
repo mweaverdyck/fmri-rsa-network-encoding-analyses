552723d84b5034ff634944310f28f24d107ac425
mweaverdyck <18310669+mweaverdyck@users.noreply.github.com>
2020-01-25 16:43:56 -0800

    add brainiak searchlight analyses

---
+++ code/level2_rsa_sl.py
@@ -0,0 +1,176 @@
+# searchlight
+from nistats.second_level_model import SecondLevelModel
+import os
+import shutil
+import sys
+from datetime import datetime
+from copy import deepcopy
+import glob
+import pandas as pd
+import nibabel as nib
+import numpy as np
+from scipy.stats import norm, ttest_rel, ttest_ind, ttest_1samp
+from statsmodels.stats.multitest import multipletests
+from nilearn.image import math_img
+from nilearn.input_data import NiftiMasker
+from nistats import second_level_model
+from nilearn import plotting
+from nilearn.plotting import plot_glass_brain
+#from nistats.second_level_model import non_parametric_inference
+from funcs import *
+
+# def correct_p (t,p):
+#     if t < 0:
+#         # if negative t-value, then relevant context was lower than non-relavant
+#         p = 1.
+#     else:
+#         p = p/2
+#     return(t,p)
+#
+# def FDR( x, n=None ):
+#     """
+#     Assumes a list or numpy array x which contains p-values for multiple tests
+#     Copied from p.adjust function from R
+#     """
+#     if n is None:
+#         n = len(x)
+#     o = [ i[0] for i in sorted(enumerate(x), key=lambda v:v[1],reverse=True) ]
+#     ro = [ i[0] for i in sorted(enumerate(o), key=lambda v:v[1]) ]
+#     q = sum([1.0/i for i in range(1,n+1)])
+#     l = [ q*len(x)/i*x[j] for i,j in zip(reversed(range(1,n+1)),o) ]
+#     l = [ l[k] if l[k] < 1.0 else 1.0 for k in ro ]
+#     return l
+
+def run_sig_tests(data_fnames):
+    second_level_model = SecondLevelModel(smoothing_fwhm=5.0)
+    if isinstance(data_fnames,dict):
+        data_fnames_num = sorted(data_fnames['number'])
+        data_fnames_fri = sorted(data_fnames['friend'])
+        fname_atts = get_all_bids_atts(data_fnames_num[0])
+        fname_atts['task'] = 'diff'
+        # create paired t-test design matrix
+        pair_mat = np.zeros((2*len(data_fnames_num),len(data_fnames_num)+1), dtype=int)
+        labs = []
+        for i in range(len(data_fnames_num)):
+            l = 's'+str(i)
+            labs = labs + [l]
+            a = [0]*len(data_fnames_num)
+            a[i] = 1
+            pair_mat[:,i] = a + a
+        pair_mat[:, len(data_fnames_num)] = [1] * len(data_fnames_num) + [-1]*len(data_fnames_fri)
+        design_matrix = pd.DataFrame(pair_mat,
+                                 columns=labs + ['diff'])
+        if fname_atts['pred'] == 'deg':
+            data_fnames = data_fnames_num + data_fnames_fri
+        elif fname_atts['pred'] == 'dist':
+            data_fnames = data_fnames_fri + data_fnames_num
+        print(data_fnames)
+        con_name = 'diff'
+    else:
+        fname_atts = get_all_bids_atts(data_fnames[0])
+        design_matrix = pd.DataFrame([1] * len(data_fnames),
+                                 columns=['intercept'])
+        con_name = 'intercept'
+    # show data files and design matrix
+    print("Running significance testing on: ")
+    print(data_fnames)
+    print("Using design matrix: ")
+    print(design_matrix)
+    # setup file names
+    del fname_atts['sub']
+    fname_atts['val2'] = "z"
+    fname_atts['correction'] = "none"
+    if 'extra' in fname_atts.keys():
+        fname_atts.move_to_end('extra')
+    # save z map
+    second_level_model = second_level_model.fit(data_fnames,
+                           design_matrix=design_matrix)
+    z_map = second_level_model.compute_contrast(con_name, output_type='z_score')
+    out_fname = os.path.join(SECOND_LEVEL_DIR, SL, make_bids_str(fname_atts))
+    nib.save(z_map, out_fname)
+    print(out_fname + ' saved.')
+    threshold = 2.88 #3.1  # correponds to  p < .001, uncorrected
+    display = plotting.plot_glass_brain(
+        z_map, threshold=threshold, colorbar=True, plot_abs=False,
+        output_file=out_fname,
+        title='z map (unc p<0.001')
+    # save p map
+    p_val = second_level_model.compute_contrast(con_name, output_type='p_value')
+    fname_atts['val2'] = "p"
+    out_fname = os.path.join(SECOND_LEVEL_DIR, SL, make_bids_str(fname_atts))
+    nib.save(p_val, out_fname)
+    print(out_fname + ' saved.')
+    # correct for multiple comparisons
+    # Correcting the p-values for multiple testing and taking negative logarithm
+    n_voxels = np.sum(second_level_model.masker_.mask_img_.get_data())
+    neg_log_pval = math_img("-np.log10(np.minimum(1, img * {}))"
+            .format(str(n_voxels)),
+            img=p_val)
+    fname_atts['val2'] = "p"
+    fname_atts['correction'] = "parametric"
+    out_fname = os.path.join(SECOND_LEVEL_DIR, SL, make_bids_str(fname_atts))
+    nib.save(neg_log_pval, out_fname)
+    print(out_fname + ' saved.')
+    # FDR correction
+    p_arr = p_val.get_data().flatten()
+    sigs, fdr_val, a, b = multipletests(p_arr, alpha=.05, method='fdr_bh', is_sorted=False, returnsorted=False)
+    fname_atts['val2'] = "p"
+    fname_atts['correction'] = "fdr"
+    out_fname = os.path.join(SECOND_LEVEL_DIR, SL, make_bids_str(fname_atts))
+    save_nii(data = fdr_val.reshape(p_val.shape), refnii=p_val, filename=out_fname)
+    # neg_log_pvals_permuted_ols_unmasked = \
+    #                     non_parametric_inference(second_level_input,
+    #                                  design_matrix=design_matrix,
+    #                                  model_intercept=True, n_perm=1000,
+    #                                  two_sided_test=False,
+    #                                  smoothing_fwhm=8.0, n_jobs=1)
+    # fname_atts['val2'] = "p"
+    # fname_atts['correction'] = "permutation"
+    # out_fname = os.path.join(SECOND_LEVEL_DIR, SL, make_bids_str(fname_atts))
+    # save_nii(neg_log_pvals_permuted_ols_unmasked, ref_img, out_fname)
+
+corrs=['spear', 'reg']
+corr_labels = []
+tasks=[]
+# read in arguments
+for arg in sys.argv[1:]:
+    print(arg)
+    if arg in corrs:
+        corr_labels += [arg]
+    elif arg in TASKS or arg == 'avg':
+        tasks += [arg]
+
+if len(corr_labels) == 0:
+    corr_labels = ['spear']
+
+if len(tasks) == 0:
+    tasks = TASKS + ['avg']
+
+task_sects = [[],[]]
+for t in tasks:
+    task_sects[t in TASKS] = task_sects[t in TASKS] + [t]
+
+out_dir = SECOND_LEVEL_DIR
+
+for corr_label in corr_labels:
+    print('starting correlation '+corr_label)
+    in_dir = os.path.join(RSA_DIR,'*',corr_label)
+    if SPACE=='T1w':
+        in_dir = os.path.join(in_dir,'T1w-2-MNI')
+    for pred in ['deg', 'dist']:
+        print('starting predictor '+pred)
+        data_tasks = {}
+        for task in tasks:
+            print("starting task "+task)
+            fnames = os.path.join(in_dir, '*task-'+task+'*space-'+SPACE+'*pred-'+pred+'*.nii*')
+            print(fnames)
+            data_fnames = glob.glob(fnames)
+            if len(data_fnames) != 0:
+                if task in TASKS:
+                    data_tasks[task] = data_fnames
+                    print(data_tasks)
+                run_sig_tests(data_fnames)
+        if pred in ['deg', 'dist'] and 'number' in data_tasks.keys() and 'friend' in data_tasks.keys():
+            run_sig_tests(data_tasks)
+
+print(str(datetime.now()) + ": End level2_rsa_sl.py")
