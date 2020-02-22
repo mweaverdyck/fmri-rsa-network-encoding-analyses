48a6d291890e3deb1332da2383ad3e0a25ea5d2b
mweaverdyck <18310669+mweaverdyck@users.noreply.github.com>
2020-02-22 13:45:49 -0800

    test for overall effect of physical, social, and social network features

---
+++ code/qsub_rsastages.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#$ -cwd
+# error = Merged with joblog
+#$ -o joblogs/joblog.rsastages.$JOB_ID.log
+#$ -j y
+#$ -pe shared 2
+#$ -l h_rt=11:59:00,h_data=8G
+# Notify when
+#$ -m ae
+#
+# runs runs searchlight RSA regression based on Nistats GLM output
+# break if error raised
+set -e
+
+# load modules and functions
+source funcs
+setup_modules $fsl_v $python_v
+
+label='RSA_STAGES'
+in_dir=${GLM_DIR}
+out_dir=${RSA_DIR}
+
+begin_script -l ${label} -i ${in_dir} -o ${out_dir} -f subid $@
+log_args="$LOG_ARGS"
+subs=( "${SUBS[@]}" )
+
+for procedure in "${PROCEDURES[@]}"; do
+  write_log $log_args "Running procedure: ${procedure}"
+  if [[ $procedure == $PARC ]]; then
+    all_parcels=( ${N_PARCELS} )
+    bash transform_mni-2-T1w.sh ${subs[@]}
+  fi
+
+  # Run RSA
+  write_log $log_args "Running RSA"
+  python3 rsa_stages.py ${procedure} ${subs[@]} | tee -a $log_file
+  write_log $log_args "Finished RSA"
+
+done
+log_end $log_args
---
+++ code/rsa_stages.py
@@ -0,0 +1,69 @@
+#!/bin/python
+from funcs import *
+from rsa_funcs import *
+
+print(str(datetime.now()) + ": Begin rsa_stages.py")
+
+# read in arguments
+all_sub = []
+tasks=[]
+# read in arguments
+for arg in sys.argv[1:]:
+    print(arg)
+    if arg in PROCEDURES_ALL:
+        procedure = arg
+    elif arg in TASKS or arg == 'avg':
+        tasks += [arg]
+    else:
+        all_sub += [arg]
+
+if len(tasks) == 0:
+    tasks = ['avg']
+
+# overwrite previous csv files? If False, will read in csv and append
+overwrite = True
+print(str(datetime.now()) + ": Analyzing " + str(len(all_sub)) + " subjects: " + str(all_sub))
+
+# set variables
+corr = 'reg' # 'corr' or 'reg'
+val_label = 'R2' # None
+
+# social network RDM dictionary
+sn_rdms = {deg_label: deg_tri, dist_label: dist_tri, dist2_label: dist2_tri}
+
+# run regression for each subject
+for s in all_sub:
+    # get correct form of subID
+    sub=convert2subid(s)
+    print(str(datetime.now()) + ": Analyzing subject %s" % sub)
+    print(str(datetime.now()) + ": Reading in data for subject " + sub)
+
+
+    # find subject's node-image mapping
+    node_order = get_node_mapping( sub )
+
+    # get model RDMs from CFD measures
+    soc_rdms = get_model_RDM_dict(node_mapping=node_order,
+                                  meas_name_array=cfd_soc, compress=False,
+                                  out_key='soc')
+    phys_rdms = get_model_RDM_dict(node_mapping=node_order,
+                                   meas_name_array=cfd_phys, compress=False,
+                                   out_key='phys')
+
+    # combine all predictor RDM dictionaries
+    all_model_rdms = {"sn":sn_rdms, "soc":soc_rdms, "phys":phys_rdms}
+
+    try:
+        procedure
+    except NameError:
+        procedures = PROCEDURES
+    else:
+        procedures = [procedure]
+    # run rsa
+    for procedure in procedures:
+        for task in tasks:
+            for key in all_model_rdms:
+                model_rdms = all_model_rdms[key]
+                run_rsa_sub(sub=sub, model_rdms=model_rdms, procedure=procedure, corr=corr, overwrite=overwrite, tasks=task, val_label=val_label, pred=key)
+
+print(str(datetime.now()) + ": End rsa_all.py")
