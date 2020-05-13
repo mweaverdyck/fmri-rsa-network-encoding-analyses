3fc82a75510998633be1ae39748724c65d729b2a
mweaverdyck <18310669+mweaverdyck@users.noreply.github.com>
2020-05-12 18:13:18 -0700

    fix naming bug

--- code/run_level2_scripts.sh
+++ code/run_level2_scripts.sh
@@ -1,7 +1,7 @@
 
 # launch all second level scripts
-qsub -N 1samp_deg qsub_level2_fsl.sh spear deg
-qsub -N 1samp_dist qsub_level2_fsl.sh spear dist
+qsub -N single_deg qsub_level2_fsl.sh spear deg
+qsub -N single_dist qsub_level2_fsl.sh spear dist
 qsub -N pair_deg qsub_level2_fsl_pairedt.sh spear deg
 qsub -N pair_dist qsub_level2_fsl_pairedt.sh spear dist
 #qsub -N pairpreds qsub_level2_fsl_pairedt_preds.sh
